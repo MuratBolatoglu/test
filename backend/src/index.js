75a709f703742800f71a088b4fb29faa1f5473a7
Murat <147882708+MuratBolatoglu@users.noreply.github.com>
2025-12-13 20:18:17 +0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -68,11 +68,12 @@ app.use("/api/albums",albumRoutes)
 app.use("/api/stats",statRoutes)
 
 
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname,"../frontend/dist")))
-    app.get("/*",(req,res) => {
-        res.sendFile(path.resolve(__dirname,"../frontend/dist/index.html"))
-    })
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.use((req, res) => {
+        res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+    });
 }
 
 app.use((err,req,res,next) => {
